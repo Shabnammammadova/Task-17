308f7c67a6c9729335106217236762b98caa3e05
shabnam <shabnamjm@code.edu.az>
2024-06-02 20:32:07 +0400

    new p element added js

--- assets/script.js
+++ assets/script.js
@@ -4,12 +4,32 @@ const liElement = document.querySelector(".li-element");
 
 function inputSection(){
     if(todoinputElement !== "" ){
-       const pElement = document.createElement("p") ;
-       pElement.textContent = todoinputElement.value;
-       liElement.append(pElement)
+    const ulElement = document.createElement("ul");
+    const liListItem = document.createElement("li");
+    liListItem.classList.add("li-element");
+
+    liListItem.innerHTML = `
+    <div class="view">
+    <div class="todo-text">
+        <div class="check-btn"></div>
+        <p>${todoinputElement.value}</p>
+    </div>
+    <button class="delete-btn">x</button>
+    </div>   `
+       ulElement.appendChild(liListItem);
+       todoList.appendChild(ulElement);
+       console.log(ulElement);
+      
     }
 }
 
-todoinputElement.addEventListener("keyup" , ()=>{
-inputSection();
+todoinputElement.addEventListener("keydown", (event)=>{
+    if(event.key === "Enter"){
+        event.preventDefault()
+        inputSection();
+    }
 })
+
+function resetInput(){
+    todoinputElement.value === ""
+}
