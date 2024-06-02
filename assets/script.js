27ac672eb03c725b789fa3c6085ff3dd4352cdb2
shabnam <shabnamjm@code.edu.az>
2024-06-02 23:09:04 +0400

    checkicon and deletebtn js added

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,31 @@
 const todoinputElement = document.querySelector(".new-todo");
 const todoList = document.querySelector(".todo-list");
 const liElement = document.querySelector(".li-element");
+const checkBtn = document.querySelector(".checkbtn");
+// const pElement = document.getElementsByTagName("p");
+const deleteBtn= document.querySelector(".delete-btn");
+
 
 function inputSection(){
-    if(todoinputElement !== "" ){
+    if(todoinputElement.value.trim() !== "" ){
     const ulElement = document.createElement("ul");
     const liListItem = document.createElement("li");
     liListItem.classList.add("li-element");
-
     liListItem.innerHTML = `
     <div class="view">
     <div class="todo-text">
-        <div class="check-btn"></div>
+    <button class="checkbtn">
+    <span class="check-icon open">
+     ✓
+    </span>
+    </button>
         <p>${todoinputElement.value}</p>
     </div>
     <button class="delete-btn">x</button>
     </div>   `
        ulElement.appendChild(liListItem);
        todoList.appendChild(ulElement);
-       console.log(ulElement);
+       
       
     }
 }
@@ -27,9 +34,24 @@ todoinputElement.addEventListener("keydown", (event)=>{
     if(event.key === "Enter"){
         event.preventDefault()
         inputSection();
+        resetInput()
     }
 })
 
 function resetInput(){
-    todoinputElement.value === ""
+    todoinputElement.value = ""
 }
+
+
+checkBtn.addEventListener("click", (event) => {
+    if (event.target.classList.contains("checkbtn")) {
+        const checkIcon = event.target.querySelector(".check-icon");
+        checkIcon.classList.toggle("open");
+       
+    }
+});
+
+deleteBtn.addEventListener("click", ()=>{
+    liElement.remove()
+})
+
